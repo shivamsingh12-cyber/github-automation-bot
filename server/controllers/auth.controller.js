ebd779edad0f1c0930f151b37cf2fe6e5bd2958e
Shivam_Singh <shivamsingh6490@gmail.com>
2026-07-09 22:20:38 +0530

    added new drizzle orm

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -27,5 +27,52 @@ export const getGithubLoginCallback= async (req,res)=>{
          "error",
          "Couldn't login with Github because of invalid login please try again!"
       )
+      return res.redirect("/login");
+   }
+
+   if(!code || !state || !storedState || state != storedState){
+      return handleFailedLogin();
+   }
+
+   let tokens;
+   try {
+      tokens=await github.validateAuthorizationCode(code);
+   } catch {
+      return handleFailedLogin();
+   }
+
+   const githubUserResponse = await fetch(`https://api.github.com/user`,{
+      headers:{
+         Authorization: `Bearer ${tokens.accessToken()}`,
+      },
+   });
+
+   if(!githubUserResponse.ok) return handleFailedLogin();
+   const githubUser=await githubUserResponse.json();
+   const {id:githubUserId,name}=githubUser;
+
+   const githubEmailResponse=await fetch("https://api.github.com/user/emails",{
+      headers:{
+         Authorization: `Bearer ${tokens.accessToken()}`,
+      },
+   });
+
+   if(!githubEmailResponse.ok) return handleFailedLogin();
+
+   const  emails = await githubEmailResponse.json();
+   const email = emails.filter((e)=>e.primary)[0].email;
+
+   if(!email) return handleFailedLogin();
+
+   let user = await getUserWithOauthID({
+      provider:"github",
+      email,
+   })
+
+   if(user && !user.providerAccountId){
+      await linkUserWithOauth({
+         userId:user.id,
+         
+      })
    }
 }
